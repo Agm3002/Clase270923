53c22afd963fdbd60f381fe046ae2e6622f02167
Luis <luisdevv232@gmail.com>
2023-09-28 10:24:48 -0700

    subiendo cambio conflictivo

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-console.log("Clase del dia 27/09/23")
-console.log("Cambio Hecho por Armando Gonzalez")
+console.log('Clase del dia 27/09/23');
+console.log('Cambio Hecho por Armando Gonzalez');
+
+console.log('cambio para crear conflicto por Luis Quiroz');
+console.log('safqefeqfdsafqe');
